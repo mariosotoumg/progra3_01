641d0bfb84dd097b6da41fbb7ba046cdab3f0066
Gerson <Gerson@DESKTOP-H2DCSJJ>
2018-02-09 21:09:02 -0600

    Notas de Descripcion de Metos

--- src/cajero/Cliente.java
+++ src/cajero/Cliente.java
@@ -9,12 +9,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.JOptionPane;
 
+/**
+ * El método Cliente, este método es utilizado para la inicialización de las variables a utilizar en la clase Cliente 
+ * 
+ * @author Gerson
+ */
+
 public class Cliente {
 
     public String nit;
     public String nombre;
     public int cheque;
     public int credito;
+    
+  /**
+   *El método Limpiar, este método es encargado de limpiar las variables utilizadas anteriormente para que así desechar datos basura que no utilizaremos y  que podrían afectar a datos resientes que deseemos ingresar     
+   *
+   * @return 
+   */  
 
     public int limpiar(){
         this.nit     = "";
@@ -23,7 +35,13 @@ public class Cliente {
         this.credito = 0;
         return 0;
     }
-
+/**
+ * El meto buscar, la funcionalidad de este método es la de ingresar un cierto parámetro ingresándolo por la variable denominada nit, este los compara en un archivo de texto, y si se ha logrado encontrado este mostrara los datos del cliente, pero si la búsqueda no es  exitosa muestra un mensaje que en el cual dice que no se ha encontrado los datos del Cliente  
+ * 
+ * @param nit
+ * @return
+ * @throws Exception 
+ */
     public int buscar(String nit) throws Exception{
         String[] registro = new String[4];
         String linea;
@@ -51,7 +69,13 @@ public class Cliente {
         }
         return encontrado;
     }
-
+    /**
+     * El método agregar, la funcionalidad de este es la de agregar un nuevo registro en este caso al archivo de texto, en esta función se deben ingresar los datos básicos que son el nit y nombre del cliente a ingresar.
+     * 
+     * @param nit
+     * @param nombre
+     * @return 
+     */
     public int agregar(String nit,String nombre){
         String linea;
         try {
--- src/cajero/Credito.java
+++ src/cajero/Credito.java
@@ -5,9 +5,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
+/**
+ * El método Crédito, la funcionalidad de esta clase es la de ver el dato que es el crédito de cada cliente a través del nit del cliente que se dese buscar  
+ * 
+ * @author Gerson
+ */
 public class Credito {
 
+    /**
+     * El método saldo, en este método se busca el saldo del cliente a través del nit para verificar si es posible que el cliente puede seguir con su crédito local o no tiene suficiente en este método tiene su propio archivo de texto 
+     * 
+     * @param nit
+     * @return
+     * @throws Exception 
+     */
     public int saldo(String nit) throws Exception{
         String[] registro = new String[3];
         String linea;
